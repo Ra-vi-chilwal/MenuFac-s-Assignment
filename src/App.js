26ce4f2ab40df376615bfb490ce87de4726bc3a3
Himanshukanwal <rvichilwal75@gmail.com>
2023-05-12 23:20:38 +0530

    update - dynamic value updation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 // import './App.css';
-import { FilterData, GammaArray } from "./components/methodFormulae/Formulae";
+import {  GammaArray ,sortSameTypeData} from "./components/methodFormulae/Formulae";
 import Table from "./components/tableComponents";
 function App() {
-  const Flavanoids1 = FilterData(1,"Flavanoids");
-  const Flavanoids2 = FilterData(2,"Flavanoids");
-  const Flavanoids3 = FilterData(3,"Flavanoids");
-  const GammaVal1 = GammaArray(FilterData(1))
-  const GammaVal2 = GammaArray(FilterData(2))
-  const GammaVal3 = GammaArray(FilterData(3))
+  const valueToCalc = sortSameTypeData("Flavanoids")
+  const gammaOfAllClasses = GammaArray()
 
   return (
     <div className="App">
@@ -19,9 +15,7 @@ function App() {
           method1={"Flavanoids Mean"}
           method2={"Flavanoids Median"}
           method3={"Flavanoids Mode"}
-        data1={Flavanoids1}
-        data2={Flavanoids2}
-        data3={Flavanoids3}
+       arrayData={valueToCalc}
         />
       </div>
       <div>
@@ -33,9 +27,7 @@ function App() {
           method1={"Gamma Mean"}
           method2={"Gamma Median"}
           method3={"Gamma Mode"}
-        data1={GammaVal1}
-        data2={GammaVal2}
-        data3={GammaVal3}
+        arrayData={gammaOfAllClasses}
         />
       </div>
     </div>
--- src/components/methodFormulae/Formulae.js
+++ src/components/methodFormulae/Formulae.js
@@ -1,21 +1,67 @@
 import WineData from "../../data.json";
 
 // this function used to filter data according to alocholClass and property of required field
-export const FilterData = ( alcoholType, item) => {
-  let filteredData = [];
-  for (let i = 0; i < WineData.length; i++) {
-    if (WineData[i].Alcohol === alcoholType) {
-        if(item){
-            filteredData.push(Number(WineData[i][item]));
-        } else{
-            filteredData.push(WineData[i]);
-        }
-    
+
+let SortArray = []
+
+export function sortSameTypeData (requiredField){
+  let SortArray = []
+ if(requiredField){
+  for(let i =0; i<WineData.length; i++){
+    let found = false;
+    for(let j=0; j<SortArray.length; j++){
+      if(SortArray[j].type === WineData[i].Alcohol){
+        SortArray[j].typeArray = [...SortArray[j].typeArray, Number(WineData[i][requiredField])]
+        found = true;
+        break;
+      }
+        
     }
+  if(found !== true){
+  SortArray.push({type:WineData[i].Alcohol, typeArray: [Number(WineData[i][requiredField])]})  
   }
-  return filteredData;
-};
-//
+    
+   }
+ } else{
+  for(let i =0; i<WineData.length; i++){
+    let found = false;
+    for(let j=0; j<SortArray.length; j++){
+      if(SortArray[j].type === WineData[i].Alcohol){
+        SortArray[j].typeArray = [...SortArray[j].typeArray, WineData[i]]
+        found = true;
+        break;
+      }
+        
+    }
+  if(found !== true){
+  SortArray.push({type:WineData[i].Alcohol, typeArray: [WineData[i]]})  
+  }
+    
+   }
+ }
+  return SortArray
+ }
+
+
+// export const FilterData = ( alcoholType, item) => {
+//   let filteredData = [];
+//   for (let i = 0; i < WineData.length; i++) {
+//     if (WineData[i].Alcohol === alcoholType) {
+//         if(item){
+//             filteredData.push(Number(WineData[i][item]));
+//         } else{
+//             filteredData.push(WineData[i]);
+//         }
+    
+//     }
+//   }
+//   return filteredData;
+// };
+
+
+
+
+
 // method for caluclating mean of array
 export const Mean = (arr) => {
   let length = arr.length;
@@ -23,12 +69,16 @@ export const Mean = (arr) => {
   let average = sum / length;
   return Number(average.toFixed(3));
 };
+
+
 // method for caluclating Median of array
 export const Median = (arr) => {
   const mid = Math.floor(arr.length / 2);
   const nums = [...arr].sort((a, b) => a - b);
   return arr.length%2!==0 ? nums[mid] : (nums[mid-1]+ nums[mid])/2
 };
+
+
 // method for caluclating mode of array
 export const Mode = (arr) => {
     const mode = {};
@@ -53,12 +103,23 @@ export const Mode = (arr) => {
     return max;
   };
 // function for caluclating Gamma of array
- export const GammaArray = (alcoholType)=>{
-    let gammaArray = []
-    for(let i =0 ; i<alcoholType.length; i++){
-        let ashAndHue = alcoholType[i].Ash * alcoholType[i].Hue
-        let gammaVal = ashAndHue/ alcoholType[i].Magnesium
-        gammaArray.push(Number(gammaVal.toFixed(3)))
+
+
+// this function return array of calculated gamma array of each type of alcohol
+ export const GammaArray = ()=>{
+    let sortedValue = sortSameTypeData()
+    let arrayOfGamma =[]
+    for(let i = 0 ; i<sortedValue.length; i++){
+      let initialArr =[]
+      for(let j =0; j<sortedValue[i].typeArray.length; j++){
+        let ashAndHue = sortedValue[i].typeArray[j].Ash * sortedValue[i].typeArray[j].Hue
+        let gammaVal = ashAndHue/sortedValue[i].typeArray[j].Magnesium
+initialArr.push(Number(gammaVal.toFixed(3)))
+      }
+      arrayOfGamma.push(initialArr)
+
     }
-    return gammaArray
+
+
+    return arrayOfGamma
   }
